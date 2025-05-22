213d4bb2c7079c53488ed1bd5a15698db6aeb3e3
Kushal425 <kushalsarkar457@gmail.com>
2025-05-22 14:50:45 +0530

    first_readyBackground

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Background from './components/background'
+
+function App() {
+
+  return (
+    <div>
+      <Background />
+      <h1>Welcome</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/background.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <div>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          minWidth: '100vw',
+          minHeight: '100vh',
+          width: '100vw',
+          height: '100vh',
+          objectFit: 'cover',
+          zIndex: -1,
+        }}
+      >
+        <source src="/X O.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  )
+}
+
+export default Background
